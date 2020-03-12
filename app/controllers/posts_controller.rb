08f76585c31b424a30df085ffe9c3b1688fcc8a1
Alisa Chang <laptite@gmail.com>
2020-03-11 21:13:28 -0700

    Add posts controller, actions, views and model

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,37 @@
+class PostsController < ApplicationController
+
+	def index	
+		@posts = Post.order(created_at: :desc)
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(post_params)
+		@post.save
+
+		redirect_to @post
+	end
+
+	def edit
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+
+		def post_params
+			params.require(:post).permit(:title, :body)
+		end
+
+end
