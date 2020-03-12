c493db9f8d7d7e626b2fa5857c8f04c243752c78
Alisa Chang <laptite@gmail.com>
2020-03-12 10:56:02 -0700

    Add post actions and add form partial

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
 
 	def index	
 		@posts = Post.order(created_at: :desc)
@@ -10,9 +11,11 @@ class PostsController < ApplicationController
 
 	def create
 		@post = Post.new(post_params)
-		@post.save
-
-		redirect_to @post
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
 	end
 
 	def edit
@@ -23,12 +26,24 @@ class PostsController < ApplicationController
 	end
 
 	def update
+		if @post.update(post_params)
+			redirect_to @post 
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
+		@post.destroy
+		redirect_to posts_path
 	end
 
 	private
+
+		def find_post
+			@posts = Post.all
+			@post = Post.find(params[:id])
+		end
 
 		def post_params
 			params.require(:post).permit(:title, :body)
